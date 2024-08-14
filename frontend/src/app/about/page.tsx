32149adc48319ac25aed98d1f09f3b5ee79ca0f3
eREDROCK <rdrc136@gmail.com>
2024-08-15 03:26:01 +0900

    feat:daisyUIを追加

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+      <div >
+        <h1 className="text-3xl font-bold underline">Hello CSS</h1>
+        <button className="btn btn-primary">Button</button>
+      </div>
+  );
+}
